8a0a5ed663448a45df7a24d605ef2ca9e317ea9c
1replica5 <antonvolsky@gmail.com>
2024-06-20 18:39:29 +0300

    Release 4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,48 @@
-import React, { useState } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Layout from './components/Layout';
-import MainPage from './components/pages/MainPage';
-import SignUp from './components/pages/SignUp';
-import axiosInstance from './components/api/axiosInstance';
+import React, { useEffect, useState } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import MainPage from "./components/pages/MainPage";
+import SignUp from "./components/pages/SignUp";
+import axiosInstance, { setAccessToken } from "./components/api/axiosInstance";
 
 function App() {
-  const [user, setUser] = useState({ status: 'fetching', data: null });
+  const [user, setUser] = useState({ status: "fetching", data: null });
+
+  useEffect(() => {
+    axiosInstance("/tokens/refresh")
+      .then(({ data }) => {
+        setUser({ status: "logged", data: data.user });
+        setAccessToken(data.accessToken);
+      })
+      .catch(() => {
+        setUser({ status: "fetching", data: null });
+        setAccessToken("");
+      });
+  }, []);
+
   const signUpHandler = (e, input) => {
     e.preventDefault();
     const formData = Object.fromEntries(new FormData(e.target));
     if (!formData.email || !formData.password || !formData.name || !input) {
-      return alert('Missing required fields');
+      return alert("Missing required fields");
     }
-    axiosInstance.post('/auth/signup', { cityId: input, ...formData }).then(({ data }) => {
-      setUser({ status: 'logged', data: data.user });
-    });
+    axiosInstance
+      .post("/auth/signup", { cityId: input, ...formData })
+      .then(({ data }) => {
+        setUser({ status: "logged", data: data.user });
+      });
   };
 
   const routes = [
     {
-      element: <Layout />,
+      element: <Layout user={user} />,
       children: [
         {
-          path: '/',
+          path: "/",
           element: <MainPage />,
         },
         {
-          path: '/auth/signup',
+          path: "/auth/signup",
           element: <SignUp signUpHandler={signUpHandler} />,
         },
         // {
--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import NavBar from './ui/NavBar';
 
-export default function Layout() {
+export default function Layout({user}) {
   return (
     <Container>
-      <NavBar />
+      <NavBar user={user} />
       <Outlet />
     </Container>
   );
